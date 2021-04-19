c468a0d815bc039c5a9f9a0f482ad8437719aafe
Mayank Rajoria <mayankrajoria3333@gmail.com>
2021-04-19 11:14:56 +0530

    added first test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+class TestURLs(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_feed_page_response(self):
+        response = self.client.get('/user/')
+        
+        self.assertEqual(response.status_code, 200)
+
+    def test_upload_page_response(self):
+        response = self.client.get('/user/upload')
+        
+        self.assertEqual(response.status_code, 200)
+        
